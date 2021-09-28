16bb9465d0281411182726125c4a9a5e71c4563c
Yahialabeeb <yahia.labeeb@gmail.com>
2021-09-28 11:48:12 +0300

    f

--- server.js
+++ server.js
@@ -15,10 +15,20 @@ server.use(cors());
 server.use(express.json());
 let port = process.env.PORT
 let mongoUrl = process.env.MONGO_URL
-server.get('/',()=>{
-    console.log('hi');
-})
 Mongoose.connect(mongoUrl)
+// Urls 
+server.get('/',()=>{
+    <h1>HELLO</h1>
+})
+server.get('/fruits',allData)
+
+
+// functions 
+function allData (req,res){
+axios.get("https://fruit-api-301.herokuapp.com/getFruit").then((result)=>{
+    res.json(result.data)
+})
+}
 server.listen(port , ()=>{
     console.log(`listen on ${port}`);
 })
